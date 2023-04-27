e2e6ffc621c8a698af75856b76c419d5e5f2b5bf
MashaKarakalcheva <126281310+MashaKarakalcheva@users.noreply.github.com>
2023-04-27 21:11:18 +0300

    Update BinarySearchTreeTest.java

--- tests3.14/src/test/java/BinarySearchTree/BinarySearchTreeTest.java
+++ tests3.14/src/test/java/BinarySearchTree/BinarySearchTreeTest.java
@@ -1,41 +1,108 @@
-import static org.junit.Assert.*;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class BinarySearchTreeTest {
+    @Test
+    public void testConstructor() {
+        BinarySearchTree bst = new BinarySearchTree();
+        assertNull(bst.root);
+    }
 
     @Test
     public void testInsert() {
         BinarySearchTree bst = new BinarySearchTree();
-        Employee emp1 = new Employee("John", "Doe", "12345");
-        Employee emp2 = new Employee("Jane", "Doe", "67890");
-        bst.insert(emp1);
-        bst.insert(emp2);
-        assertEquals(bst.search("12345"), emp1);
-        assertEquals(bst.search("67890"), emp2);
+        Employee employee1 = new Employee("Иван", "Матвиенко", "Секретарь", "Средний", 5, 1234567890L);
+        Employee employee2 = new Employee("Юлия", "Сидорова", "Менеджер", "Низкий", 3, 2345678901L);
+        bst.insert(employee1);
+        bst.insert(employee2);
+        assertEquals(employee1, bst.root.employee);
+        assertEquals(employee2, bst.root.right.employee);
     }
 
     @Test
     public void testDelete() {
         BinarySearchTree bst = new BinarySearchTree();
-        Employee emp1 = new Employee("John", "Doe", "12345");
-        Employee emp2 = new Employee("Jane", "Doe", "67890");
-        bst.insert(emp1);
-        bst.insert(emp2);
-        bst.delete("12345");
-        assertNull(bst.search("12345"));
-        assertEquals(bst.search("67890"), emp2);
+        Employee employee1 = new Employee("Иван", "Петрова", "Тестировщик", "Средний", 5, 1234567890L);
+        Employee employee2 = new Employee("Юлия", "Попова", "Ассистент", "Низкий", 3, 2345678901L);
+        Employee employee3 = new Employee("Анна", "Сидорова", "Разработчик", "Высокий", 7, 3456789012L);
+        bst.insert(employee1);
+        bst.insert(employee2);
+        bst.insert(employee3);
+        bst.delete(2345678901L);
+        assertNull(bst.search(2345678901L));
+        assertEquals(employee1, bst.root.employee);
+        assertEquals(employee3, bst.root.right.employee);
     }
 
     @Test
     public void testSearch() {
         BinarySearchTree bst = new BinarySearchTree();
-        Employee emp1 = new Employee("John", "Doe", "12345");
-        Employee emp2 = new Employee("Jane", "Doe", "67890");
-        bst.insert(emp1);
-        bst.insert(emp2);
-        assertEquals(bst.search("12345"), emp1);
-        assertEquals(bst.search("67890"), emp2);
-        assertNull(bst.search("99999"));
-    }
+        Employee employee1 = new Employee("Анна", "Иванова", "Дизайнер", "Высокий", 5, 1234567890L);
+        Employee employee2 = new Employee("Кира", "Кошкина", "Aссистент", "Низкий", 2, 1234567891L);
+Employee employee3 = new Employee("Алексей", "Алексеев", "Инженер", "Высокий", 10, 1234567892L);
+bst.insert(employee1);
+bst.insert(employee2);
+bst.insert(employee3);
+    
+    assertEquals(bst.search(1234567890L).employee, employee1);
 
+    assertNull(bst.search(1234567893L));
+}
+
+@Test
+public void testInOrder() {
+    BinarySearchTree bst = new BinarySearchTree();
+    Employee employee1 = new Employee("Анна", "Иванова", "Дизайнер", "Высокий", 5, 1234567890L);
+    Employee employee2 = new Employee("Кира", "Кошкина", "Aссистент", "Низкий", 2, 1234567891L);
+    Employee employee3 = new Employee("Алексей", "Алексеев", "Инженер", "Высокий", 10, 1234567892L);
+    bst.insert(employee1);
+    bst.insert(employee2);
+    bst.insert(employee3);
+
+    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(outContent));
+    bst.InOrder();
+    assertEquals(outContent.toString(), "Анна, Иванова, Дизайнер, Высокий, 5, 1234567890 \n" +
+            "Кира Кошкина, Aссистент, Низкий, 2, 12345678910 \n" +
+            "Алексей, Алексеев, Инженер, Высокий, 10, 1234567892 \n");
+    System.setOut(System.out);
+}
+
+@Test
+public void testPreOrder() {
+    BinarySearchTree bst = new BinarySearchTree();
+    Employee employee1 = new Employee("Анна", "Иванова", "Дизайнер", "Высокий", 5, 1234567890L);
+    Employee employee2 = new Employee("Кира", "Кошкина", "Aссистент", "Низкий", 2, 1234567891L);
+    Employee employee3 = new Employee("Алексей", "Алексеев", "Инженер", "Высокий", 10, 1234567892L);
+    bst.insert(employee1);
+    bst.insert(employee2);
+    bst.insert(employee3);
+
+    // Test PreOrder traversal
+    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(outContent));
+    bst.PreOrder(bst.root);
+    assertEquals(outContent.toString(), "Анна, Иванова, Дизайнер, Высокий, 5, 1234567890 \n" +
+            "Кира Кошкина, Aссистент, Низкий, 2, 12345678910 \n" +
+            "Алексей, Алексеев, Инженер, Высокий, 10, 1234567892 \n");
+    System.setOut(System.out);
+}
+
+@Test
+public void testPostOrder() {
+    BinarySearchTree bst = new BinarySearchTree();
+    Employee employee1 = new Employee("Анна", "Иванова", "Дизайнер", "Высокий", 5, 1234567890L);
+    Employee employee2 = new Employee("Кира", "Кошкина", "Aссистент", "Низкий", 2, 1234567891L);
+    Employee employee3 = new Employee("Алексей", "Алексеев", "Инженер", "Высокий", 10, 1234567892L);
+    bst.insert(employee1);
+    bst.insert(employee2);
+    bst.insert(employee3);
+
+   ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+System.setOut(new PrintStream(outContent));
+bst.PostOrder(bst.root);
+assertEquals(outContent.toString(), "Анна, Иванова, Дизайнер, Высокий, 5, 1234567890 \n" +
+            "Кира Кошкина, Aссистент, Низкий, 2, 12345678910 \n" +
+            "Алексей, Алексеев, Инженер, Высокий, 10, 1234567892 \n");
+    System.setOut(System.out);
 }
